82178057293494311855ca8517ae44cc81a5b030
NiveditaBhagat <bhagatnivedita469@gmail.com>
2026-04-10 17:24:39 +0530

    Initial project setup with auth structure

---
+++ app/core/config.py
@@ -0,0 +1,10 @@
+# Environment variables (DB URL, JWT Secret Key)
+
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+# SECRET_KEY = os.getenv("SECRET_KEY")
---
+++ app/models/user_model.py
@@ -0,0 +1,30 @@
+# Database Tables (SQLAlchemy Models)
+# Table for Users (Email, Hashed Password, Role) for authentication
+
+from decimal import Clamped
+import email
+import enum
+from app.database import Base
+from sqlalchemy.sql import func
+from sqlalchemy import Column,Integer, String, Boolean,DateTime,Enum
+
+
+# Role Enum
+class UserRole(str, enum.Enum):
+    BORROWER = "BORROWER"
+    CREDIT = "CREDIT"
+    OPS = "OPS"
+    ADMIN = "ADMIN"
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    role = Column(Enum(UserRole), nullable=False)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+
