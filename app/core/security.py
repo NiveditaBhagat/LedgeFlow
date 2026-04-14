e4d743befa9d5f1f865b578391aca88ffa2c682a
NiveditaBhagat <bhagatnivedita469@gmail.com>
2026-04-14 22:31:26 +0530

    jwt token

--- app/core/security.py
+++ app/core/security.py
@@ -2,6 +2,19 @@
 
 from passlib.context import CryptContext
 from passlib.context import CryptContext
+from app.core.config import SECRET_KEY
+from datetime import datetime, timedelta, timezone
+from jose import jwt
 
 
 bcrypt_context=CryptContext(schemes=['bcrypt'],deprecated='auto')
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+
+#incomplete
+def create_access_token(username: str,  user_id: int,role: str, expires_delta: timedelta):
+    encode={'sub': username, 'id': user_id,'role':role}
+    expires=datetime.now(timezone.utc)+expires_delta
+    encode.update({'exp':expires})
+    return jwt.encode(encode,SECRET_KEY,algorithm=ALGORITHM)
