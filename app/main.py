e489894bc85c98fac0f1597ed7f0f66556534f96
NiveditaBhagat <bhagatnivedita469@gmail.com>
2026-04-30 23:30:21 +0530

    loan application apply api

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI
 from app.database import Base
 from app.database import engine
 from app.models import user_model
-from app.routers import auth_router, user_profile_router, kyc_router
+from app.routers import auth_router, user_profile_router, kyc_router,loan_router
 from app.schemas import loan_schema
 
 app = FastAPI()
@@ -14,5 +14,5 @@ Base.metadata.create_all(bind=engine)
 app.include_router(auth_router.router)
 app.include_router(user_profile_router.router)
 app.include_router(kyc_router.router)
-
+app.include_router(loan_router.router)
 
--- app/routers/loan_router.py
+++ app/routers/loan_router.py
@@ -8,8 +8,10 @@ from fastapi import APIRouter, Depends,HTTPException
 from app.models.user_profile_model import UserProfile,KYCStatus
 from app.models.loan_application_model import LoanApplication
 from app.models.user_model import UserRole
-from app.schemas.loan_schema import LoanApplicationRequest
+from app.schemas.loan_schema import LoanApplicationRequest,LoanApplyResponse
 from starlette import status
+from app.enums.loan_enums import  LoanStatus
+from datetime import datetime
 
 router=APIRouter(
     prefix='/loans',
@@ -22,13 +24,75 @@ user_dependency=Annotated[dict,Depends(get_current_user)]
 
 @router.post("/apply",status_code=status.HTTP_201_CREATED)
 async def apply_loan( loan_request: LoanApplicationRequest,db: db_dependency,current_user:user_dependency):
+
     if current_user.role != UserRole.BORROWER:
         raise HTTPException(
             status_code=403,
             detail="Only borrowers can apply for loans"
         )
+    
     if not loan_request.consent_given:
         raise HTTPException(
             status_code=400, detail="Consent is required to proceed"
         )
+    
+    loan = LoanApplication(
+        user_id=current_user.id,
+
+        # --- Standard Info ---
+        full_name=loan_request.full_name,
+        mobile=loan_request.mobile,
+
+        loan_type=loan_request.loan_type,
+        requested_amount=loan_request.requested_amount,
+        tenure_months=loan_request.tenure_months,
+        interest_rate_type=loan_request.interest_rate_type,
+
+        # --- Financial Snapshot ---
+        monthly_income=loan_request.monthly_income,
+        employment_type=loan_request.employment_type,
+        organization_name=loan_request.organization_name,
+        existing_monthly_obligations=loan_request.existing_monthly_obligations,
+
+        # --- Vehicle ---
+        vehicle_make=loan_request.vehicle_make,
+        vehicle_model=loan_request.vehicle_model,
+        is_used_vehicle=loan_request.is_used_vehicle,
+        vehicle_registration_number=loan_request.vehicle_registration_number,
+
+        # --- Gold ---
+        gold_weight_grams=loan_request.gold_weight_grams,
+        gold_purity=loan_request.gold_purity,
+
+        # --- Business ---
+        business_name=loan_request.business_name,
+        business_type=loan_request.business_type,
+        gst_number=loan_request.gst_number,
+        business_vintage_years=loan_request.business_vintage_years,
+
+        # --- Home ---
+        property_type=loan_request.property_type,
+        property_address_line_1=loan_request.property_address_line_1,
+        property_address_line_2=loan_request.property_address_line_2,
+        property_city=loan_request.property_city,
+        property_state=loan_request.property_state,
+        property_pincode=loan_request.property_pincode,
+        estimated_property_value=loan_request.estimated_property_value,
+
+        # --- Consent ---
+        consent_given=True,
+        consent_timestamp=datetime.utcnow(),
+
+        # --- Status ---
+        status=LoanStatus.INITIATED
+    )
+
+    db.add(loan)
+    db.commit()
+    db.refresh(loan)
+
+    return LoanApplyResponse(
+        message="Loan application submitted successfully",
+        loan_id=loan.id
+    )
     
